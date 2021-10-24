2a3339d3a1c7de2fc2baf9bc6b42a4dfebcb0495
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2021-10-24 23:51:11 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,4 @@
-input.onGesture(Gesture.Shake, function () {
-    if (PåAv == 0) {
-        PåAv = 1
-    } else {
-        PåAv = 0
-    }
-})
-radio.onReceivedValue(function (name, value) {
-    if (name == "P") {
-        PåAvBil = value
-    } else if (name == "A") {
-        Venstre = value
-    } else if (name == "B") {
-        Høyre = value
-    } else if (name == "H") {
-        Kjør = value * -22.5
-    }
-})
-let Knapp_B = 0
-let Knapp_A = 0
-let Hastighet = 0
-let Kjør = 0
-let Høyre = 0
-let Venstre = 0
-let PåAvBil = 0
-let PåAv = 0
-radio.setGroup(1)
-bitbot.ledRainbow()
-basic.forever(function () {
+function Fjernkontroll () {
     Hastighet = input.rotation(Rotation.Pitch)
     if (input.buttonIsPressed(Button.A)) {
         Knapp_A = 1
@@ -38,21 +10,61 @@ basic.forever(function () {
     } else {
         Knapp_B = 0
     }
+}
+function Sende_Info () {
     radio.sendValue("H", Hastighet)
     radio.sendValue("A", Knapp_A)
     radio.sendValue("B", Knapp_B)
     radio.sendValue("P", PåAv)
-    if (PåAvBil == 1) {
-        if (Venstre == 1) {
+}
+function BitBot_Bilen () {
+    if (PåAv_Bil) {
+        if (Venstre) {
             bitbot.motor(BBMotor.Right, Kjør)
             bitbot.motor(BBMotor.Left, Kjør * -1)
-        } else if (Høyre == 1) {
+        } else if (Høyre) {
             bitbot.motor(BBMotor.Left, Kjør)
             bitbot.motor(BBMotor.Right, Kjør * -1)
         } else {
             bitbot.motor(BBMotor.Both, Kjør)
         }
     } else {
-        bitbot.motor(BBMotor.Both, 0)
+        bitbot.motor(BBMotor.Both, Stopp)
     }
+}
+input.onGesture(Gesture.Shake, function () {
+    if (PåAv) {
+        PåAv = 0
+    } else {
+        PåAv = 1
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "H") {
+        Kjør = value
+    } else if (name == "A") {
+        Venstre = value
+    } else if (name == "B") {
+        Høyre = value
+    } else if (name == "P") {
+        PåAv_Bil = value * -22.5
+    }
+})
+let Kjør = 0
+let Høyre = 0
+let Venstre = 0
+let PåAv_Bil = 0
+let PåAv = 0
+let Knapp_B = 0
+let Knapp_A = 0
+let Hastighet = 0
+let Radiogruppe = 0
+let Stopp = 0
+Stopp = 0
+radio.setGroup(Radiogruppe)
+bitbot.ledRainbow()
+basic.forever(function () {
+    Fjernkontroll()
+    Sende_Info()
+    BitBot_Bilen()
 })
