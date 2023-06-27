d164ff61650133021604fd200b6cfd849615504f
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2023-06-27 15:29:24 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,56 @@
 function Fjernkontroll () {
-    Hastighet = input.rotation(Rotation.Pitch)
-    if (input.buttonIsPressed(Button.A)) {
-        Knapp_A = 1
-    } else {
-        Knapp_A = 0
+    if (input.buttonIsPressed(Button.AB)) {
+        if (PåAv) {
+            PåAv = 0
+            basic.showIcon(IconNames.No)
+        } else {
+            PåAv = 1
+            basic.showIcon(IconNames.Yes)
+        }
     }
-    if (input.buttonIsPressed(Button.B)) {
-        Knapp_B = 1
-    } else {
-        Knapp_B = 0
+    if (PåAv == 1) {
+        Hastighet = input.rotation(Rotation.Pitch)
+        if (input.buttonIsPressed(Button.A)) {
+            Knapp_A = 1
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `,0)
+        } else {
+            Knapp_A = 0
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Knapp_B = 1
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `,0)
+        } else {
+            Knapp_B = 0
+        }
+        if (Knapp_A == 0 && Knapp_B == 0 && Hastighet < 0) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `,0)
+        } else if (Knapp_A == 0 && Knapp_B == 0 && Hastighet > 0) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `,0)
+        }
     }
 }
 function Sende_Info () {
@@ -32,13 +74,6 @@ function BitBot_Bilen () {
         bitbot.motor(BBMotor.Both, Stopp)
     }
 }
-input.onGesture(Gesture.Shake, function () {
-    if (PåAv) {
-        PåAv = 0
-    } else {
-        PåAv = 1
-    }
-})
 radio.onReceivedValue(function (name, value) {
     if (name == "P") {
         PåAv_Bil = value
@@ -54,15 +89,16 @@ let Kjør = 0
 let Høyre = 0
 let Venstre = 0
 let PåAv_Bil = 0
-let PåAv = 0
 let Knapp_B = 0
 let Knapp_A = 0
 let Hastighet = 0
+let PåAv = 0
 let Stopp = 0
 let Radiogruppe = 42
 Stopp = 0
 radio.setGroup(Radiogruppe)
 bitbot.ledRainbow()
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     Fjernkontroll()
     Sende_Info()
